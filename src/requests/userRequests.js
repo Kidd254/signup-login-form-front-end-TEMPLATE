bfac6dab0b6dbc80ae5f7f7e3137015ffb993809
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-01-11 11:59:29 +0300

    Create post requests for registration and login

---
+++ src/requests/userRequests.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+const baseUrl = process.env.REACT_APP_API_URL;
+
+const userRequests = {};
+userRequests.createUsers = function(userData){
+    return axios.post(`${baseUrl}/users`, userData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        return response;
+    }).catch((error) => {
+        return error;
+    });
+    
+}
+
+userRequests.loginusers = function(userData){
+    return axios.post(`${baseUrl}/users/login`, userData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        return response;
+    }).catch((error) => {
+        return error;
+    });
+    
+}
